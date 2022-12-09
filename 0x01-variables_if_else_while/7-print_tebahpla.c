4815ccd54b564e72448686b5ec7037aa8906caca
Faithchep414 <faithchepkoech414@gmail.com>
2022-12-09 15:26:20 -0800

    Print

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *
+ * main -Entry point
+ *
+ * Description: Print lowercase alphabet backwards
+ * Return: 0
+ */
+int main()
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; --c)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
