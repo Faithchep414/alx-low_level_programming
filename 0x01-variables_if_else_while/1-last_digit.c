81a2b5fb3028fe5543b73a37b79ee2e08df5a4e7
Faithchep414 <faithchepkoech414@gmail.com>
2022-12-09 13:57:29 -0800

    variables

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+
+
+int main()
+
+{
+
+	    int n;
+
+
+
+	        /* initialize random seed: */
+
+	        srand (time(NULL));
+
+
+
+		    /* generate secret number between 0 and RAND_MAX: */
+
+		    n = rand() % RAND_MAX;
+
+
+
+		        int lastDigit = n % 10;
+
+
+
+			    printf("Last digit of %d is %d ", n, lastDigit);
+
+
+
+			        if (lastDigit > 5) {
+
+					        printf("and is greater than 5\n");
+
+						    } else if (lastDigit == 0) {
+
+							            printf("and is 0\n");
+
+								        } else {
+
+										        printf("and is less than 6 and not 0\n");
+
+											    }
+
+
+
+				    return 0;
+
+}
