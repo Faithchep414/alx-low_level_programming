156e84e726ca38d1e6cc3f0d9fcb31cd310c1503
Faithchep414 <faithchepkoech414@gmail.com>
2022-12-12 13:38:37 -0800

    Variables

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,33 +1,20 @@
 #include <stdio.h>
+
 /**
  *
- *  * main - entry point
- *
- *   *
- *
- *    * Description: Print the lowercase alphabet
- *
- *     * Return: 0
- *
- *      */
+ * main - entry point
+ * Description: Print the lowercase alphabet
+ * Return: Always 0
+ */
 int main(void)
-
 {
-
-	    char letter;
-
-
-
-	        for (letter = 'a'; letter <= 'z'; letter++) {
-
-			        putchar(letter);
-
-				    }
-
-		    putchar('\n');
-
-
-
-		        return 0;
-
+	char letter;
+        
+	
+	for (letter = 'a'; letter <= 'z'; letter++);
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+        return (0);
 }
