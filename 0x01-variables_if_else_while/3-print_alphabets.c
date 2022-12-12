4bdd360b8ba5c69921a6c1e5a7f348ec02e58da5
Faithchep414 <faithchepkoech414@gmail.com>
2022-12-12 13:57:56 -0800

    Variable

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 
 /**
- *
  * main - entry point
- *
- * Description: Print lower and upper alphabet on the same line
- *
  * Return: Always 0
  */
 int main(void)
 {
 	char ch;
 
-	for (ch = 'a'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 
-	for (ch = 'A'; ch<= 'Z'; ch++)
+	for (ch = 'A'; ch <= 'Z'; ch++)
 		putchar (ch);
 
 	putchar('\n');
