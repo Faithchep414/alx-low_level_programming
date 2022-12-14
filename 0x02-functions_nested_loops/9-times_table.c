9fd6bb6f50c3b97140f89871537181d246dfd786
Faithchep414 <faithchepkoech414@gmail.com>
2022-12-13 17:47:51 -0800

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,7 +6,7 @@
 void times_table(void)
 {
 	int num, mult, prod;
-	
+
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0')
@@ -15,10 +15,10 @@ void times_table(void)
 		{
 			_putchar(',');
 			_putchar(' ');
-			
-			
+
+
 			prod = num * mult;
-			
+
 			if (prod <= 9)
 				_putchar(' ');
 			else
