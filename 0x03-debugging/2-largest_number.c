a52c353f0993d36fae15b3c7ae530f58b0243292
Faithchep414 <faithchepkoech414@gmail.com>
2022-12-17 09:04:00 -0800

    2 task

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,18 +12,23 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b && a > c)
+if (a > b)
 {
+if (b > c)
 	largest = a;
-}
-else if (c > b)
-{
+else if (a > c)
+	largest = a;
+else
 	largest = c;
+}
+else
+{
+	if (b < c)
+		largest = c;
 }
 else
 {
 	largest = b;
 }
-
 return (largest);
 }
