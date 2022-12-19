302af7649b8d8c846346c44a4d1f96435fe78f77
Faithchep414 <faithchepkoech414@gmail.com>
2022-12-18 23:28:02 -0800

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+include "main.c"
+/**
+ * _strlen - returns the length of a full string
+ * @s: string
+ * Return; length
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while  (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	return (longi)
+}
