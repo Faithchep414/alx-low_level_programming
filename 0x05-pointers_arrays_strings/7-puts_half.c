7249da5f34471af3c0a8eb575b8f679f44906c2f
Faithchep414 <faithchepkoech414@gmail.com>
2022-12-20 04:09:34 -0800

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,22 +1,19 @@
 #include "main.h"
-#include <string.h>
 /**
  * puts_half - prints_half of a string
  * @str: string to be printed
  */
 void puts_half(char *str)
 {
-	int i length;
+	int len;
 
-	length = strlen(str);
-
-	i = length % 2 ? (length - 1) / 2 : length / 2;
-
-	while (str[i] != '\0')
+	if (str)
+	
 	{
-		_putchar(str[i]);
-		i++;
+		for (len = 0; *(str + len); ++len)
+			;
+		for (str += (len + 1) / 2; *str; ++str)
+			_putchar(*str);
+		_putchar('\n');
 	}
-
-	 _putchar('\n');
 }
